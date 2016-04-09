131a98425c068fca77cc13a35f85bae40921ba46
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2016-04-09 11:33:00 +0100

    Core classes and enumerations

---
+++ LaTeX/LaTeXCommandName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTeX
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class LaTeXCommandName : Attribute
+    {
+        public string Name { get; private set; }
+
+        public LaTeXCommandName(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ LaTeX/LaTeXEnd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTeX
+{
+    [LaTeXCommandName("end")]
+    public class LaTeXEnd : LaTeXCommand
+    {
+        public LaTeXEnvironments Environment { get; set; }
+    }
+}

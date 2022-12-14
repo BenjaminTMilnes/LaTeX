c3832257184c8be4e42efea5303037b7f245c348
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2022-12-14 00:13:43 +0000

    Began adding LaTeXWriter

---
+++ LaTeX/LaTeX.TerminalApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LaTeX.TerminalApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command1 = new LaTeXDocumentClassCommand();
+            var command2 = new LaTeXBeginCommand();
+            var command3 = new LaTeXEndCommand();
+
+            command2.Environment = LaTeXEnvironment.Document;
+            command3.Environment = LaTeXEnvironment.Document;
+
+            using (var latexWriter = new LaTeXWriter(@"..\..\..\..\output.tex"))
+            {
+                latexWriter.WriteCommand(command1);
+                latexWriter.WriteCommand(command2);
+                latexWriter.WriteCommand(command3);
+            }
+        }
+    }
+}
---
+++ LaTeX/LaTeX/LaTeXWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LaTeX
+{
+    public class LaTeXWriter : IDisposable
+    {
+        protected StreamWriter _streamWriter;
+
+        public LaTeXWriter(string filePath)
+        {
+            _streamWriter = new StreamWriter(filePath);
+        }
+
+        public void WriteCommand(LaTeXCommand command)
+        {
+            if (command is LaTeXDocumentClassCommand)
+            {
+                var c = command as LaTeXDocumentClassCommand;
+
+                _streamWriter.Write("\\documentclass{book}\n\n");
+            }
+            else if (command is LaTeXBeginCommand)
+            {
+                var c = command as LaTeXBeginCommand;
+
+                if (c.Environment == LaTeXEnvironment.Document)
+                {
+                    _streamWriter.Write("\\begin{document}\n");
+                }
+            }
+            else if (command is LaTeXEndCommand)
+            {
+                var c = command as LaTeXEndCommand;
+
+                if (c.Environment == LaTeXEnvironment.Document)
+                {
+                    _streamWriter.Write("\\end{document}\n");
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _streamWriter.Dispose();
+        }
+    }
+}
